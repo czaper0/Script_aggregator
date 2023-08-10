0cdb7e516d3e385c1df77d53c0c0e30e5e2c0140
Kacper Murat <kacpermurat@gmail.com>
2023-08-10 14:25:32 +0200

    dark mode

---
+++ scripts/error.py
@@ -0,0 +1,7 @@
+def main():
+    print("Starting the script...")
+    result = 10 / 0  # To spowoduje błąd dzielenia przez zero
+    print("Script finished!")
+
+if __name__ == "__main__":
+    main()
--- scripts/script2.py
+++ scripts/script2.py
@@ -1,1 +1,6 @@
+# Title: Wypisywanie hello world
+# Description: Skrypt wypisuje hello world na ekranie
+# Parameters: 
+#
+
 print('Wynik skryptu 2')
