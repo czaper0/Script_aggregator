a6f6a0abc155587671e6246ca54d8a690729a239
Kacper Murat <kacpermurat@gmail.com>
2023-08-11 14:13:46 +0200

    Drobne poprawki, wszystko dziala, kolejny krok - zapisywanie wynikow do plikow

--- scripts/Kategoria_1/script2.py
+++
@@ -1,6 +0,0 @@
-# Title: Wypisywanie hello world
-# Description: Skrypt wypisuje hello world na ekranie
-# Parameters: 
-#
-
-print('Wynik skryptu 2')
---
+++ scripts/Przypadki testowe/delay_2s.py
@@ -0,0 +1,9 @@
+# Title: Wypisywanie hello world
+# Description: Skrypt wypisuje hello world na ekranie
+# Parameters: 
+#
+from time import sleep
+from datetime import datetime
+print('START',datetime.now())
+sleep(2)
+print('DZIALA',datetime.now())
